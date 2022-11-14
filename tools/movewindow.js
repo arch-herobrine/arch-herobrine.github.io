084567d7da7b463fea3190a06def43c5918be3d9
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2022-11-14 13:25:38 +0900

    edit

--- tools/movewindow.js
+++ tools/movewindow.js
@@ -1,30 +1,33 @@
 class archMoveWindow {
-    getInfo() {
-      return {
-        id: 'archMoveWindow', // change this if you make an actual extension!
-        name: 'Move Window',
-        blocks: [
-          {
-            opcode: 'moveto',
-            blockType: Scratch.BlockType.COMMAND,
-            text: 'window.moveBy([ONE],[TWO])',
-            arguments: {
-              ONE: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: 0
-              },
-              TWO: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: 0
-              }
+  getInfo() {
+    return {
+      id: 'archMoveWindow', // change this if you make an actual extension!
+      name: 'Move Window',
+      blocks: [
+        {
+          opcode: 'moveto',
+          blockType: Scratch.BlockType.COMMAND,
+          text: 'window.moveBy([ONE],[TWO])',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 0
+            },
+            TWO: {
+              type: Scratch.ArgumentType.NUMBER,
+              defaultValue: 0
             }
           }
-        ]
-      };
-    }
-    moveto(args) {
-      window.moveBy(args.ONE,args.TWO)
-      setTimeout(void(0),33)
-    }
+        }
+      ]
+    };
   }
-  Scratch.extensions.register(new archMoveWindow());
+  moveto(args) {
+    (async function () {
+      window.moveBy(args.ONE, args.TWO)
+      setTimeout(void (0), 33)
+    }
+    )()
+  }
+}
+Scratch.extensions.register(new archMoveWindow());
