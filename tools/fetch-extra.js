a82470395b8651ee711037cc21127ecc8e06c49c
arch-herobrine <112319383+arch-herobrine@users.noreply.github.com>
2023-07-31 21:56:56 +0900

    Create fetch-extra.js

---
+++ tools/fetch-extra.js
@@ -0,0 +1,35 @@
+class archExFetch {
+  getInfo() {
+    return {
+      id: 'archExFetch',
+      name: 'Fetch module by arch-herobrine',
+      blocks: [
+        {
+          opcode: 'archfetch',
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'await fetch([ONE],[TWO])',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "https://turbowarp.com"
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: "{}"
+            }
+          }
+        }
+      ]
+    }
+  }
+  archFetch(args){
+    try{
+      const res = await fetch(args.ONE,JSON.parse(args.TWO));
+      return JSON.stringify({status:res.status,body:(await res.text())});
+    }catch(e){
+      return e;
+    }
+  }
+  
+}
+Scratch.extensions.register(new archMoveWindow());
