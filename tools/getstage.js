90951c17c0892777788a4f00c39b494a4a421cc8
arch.msi <112319383+arch-herobrine@users.noreply.github.com>
2024-05-04 19:51:02 +0900

    aaa

--- tools/getstage.js
+++ tools/getstage.js
@@ -1,21 +1,27 @@
-class archMoveWindow {
-  getInfo() {
-    return {
-      id: 'getStage',
-      name: 'getStage',
-      blocks: [
-        {
-          opcode: "getStageDataURL",
-          blockType: Scratch.BlockType.REPORTER,
-          text: 'getStageDataURL'
-        }
-      ]
-    }
-  }
-  getStageDataURL(args) {
-    console.log(Scratch);
-    return Scratch.renderer.canvas.toDataURL();
+(function(Scratch){
+  if (!Scratch.extensions.unsandboxed) {
+    throw new Error("This extension must run unsandboxed");
   }
   
-}
-Scratch.extensions.register(new archMoveWindow());
+  class getStage {
+    getInfo() {
+      return {
+        id: 'getStage',
+        name: 'getStage',
+        blocks: [
+          {
+            opcode: "getStageDataURL",
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'getStageDataURL'
+          }
+        ]
+      }
+    }
+    getStageDataURL(args) {
+      console.log(Scratch);
+      return Scratch.renderer.canvas.toDataURL();
+    }
+    
+  }
+  Scratch.extensions.register(new getStage());
+})
