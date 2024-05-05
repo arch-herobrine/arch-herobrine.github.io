57b9d8c933d4bf8df48e423d954eeababff59b98
arch.msi <112319383+arch-herobrine@users.noreply.github.com>
2024-05-05 11:22:58 +0900

    aaa

--- tools/getstage.js
+++ tools/getstage.js
@@ -1,27 +1,25 @@
-(function(Scratch){
-  if (!Scratch.extensions.unsandboxed) {
-    throw new Error("This extension must run unsandboxed");
-  }
+if (!Scratch.extensions.unsandboxed) {
+  throw new Error("This extension must run unsandboxed");
+}
 
-  class getStage {
-    getInfo() {
-      return {
-        id: 'getStage',
-        name: 'getStage',
-        blocks: [
-          {
-            opcode: "getStageDataURL",
-            blockType: Scratch.BlockType.REPORTER,
-            text: 'getStageDataURL'
-          }
-        ]
-      }
+class getStage {
+  getInfo() {
+    return {
+      id: 'getStage',
+      name: 'getStage',
+      blocks: [
+        {
+          opcode: "getStageDataURL",
+          blockType: Scratch.BlockType.REPORTER,
+          text: 'getStageDataURL'
+        }
+      ]
     }
-    getStageDataURL(args) {
-      Scratch.vm.runtime.renderer.draw();
-      return Scratch.renderer.canvas.toDataURL();
-    }
-    
   }
-  Scratch.extensions.register(new getStage());
-})(Scratch)
+  getStageDataURL(args) {
+    Scratch.vm.runtime.renderer.draw();
+    return Scratch.renderer.canvas.toDataURL();
+  }
+  
+}
+Scratch.extensions.register(new getStage());
